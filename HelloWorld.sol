1ce43b14f981edc1416b59651f96c0a57ee919e1
Walt <connor21@rams.colostate.edu>
2022-03-20 21:18:31 -0600

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.7.0;
 
 contract HelloWorld {
 
-    // Pure functions ensure that the function does not read or modify the state.
+    // Pure functions ensures that the function does not read or modify the state.
     function get() public pure returns (string memory){
         return 'Hello World';
     }

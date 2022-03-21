51fb956873e43e5ae606ada9c501987273712366
Walt <connor21@rams.colostate.edu>
2022-03-20 21:08:02 -0600

    Create ArrayManager.sol

---
+++ ArrayManager.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.10;
+
+contract ArrayManager{
+    uint[] public Randoms; 
+    uint[] public Rands = [4, 200, 7, 6, 8, 0, 1, 113];
+
+    function GetItemRandoms(uint index) public view returns(uint){
+        return Randoms[index];
+    }
+
+    function GetRandoms() public view returns(uint[] memory){
+        return Randoms;
+    }
+
+    function PushToRandoms(uint value) public{
+        Randoms.push(value);
+    }
+
+    function PopFromRandoms() public returns(uint[] memory){
+        Randoms.pop();
+        return Randoms;
+    }
+
+    function RemoveFromRandoms(uint index) public{
+        delete Randoms[index];
+    }
+
+    function GetRandomsLength() public view returns(uint){
+        return Randoms.length;
+    } 
+}

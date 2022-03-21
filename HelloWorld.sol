26233a86c1e77458af91e3366431a6fbed0b1d5e
Walt <connor21@rams.colostate.edu>
2022-03-20 21:18:20 -0600

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,8 +3,7 @@ pragma solidity >=0.5.0 <0.7.0;
 
 contract HelloWorld {
 
-    // pure means you cannot change the state in the function 
-    // and denies the ability to read the state or storage variables.
+    // Pure functions ensure that the function does not read or modify the state.
     function get() public pure returns (string memory){
         return 'Hello World';
     }
